d78ffa2aef0597c857fd2f60cceaa0a0e3298b3b
Jeffrey Huang <71650487+Jeffrey-Huang11@users.noreply.github.com>
2020-12-11 20:39:07 -0500

    updated 15

---
+++ 15_flask-sess/app.py
@@ -0,0 +1,29 @@
+# Team Elephant (Jeffrey Huang, Matthew Hui, Winnie Huang, Ethan Machleder)
+# SoftDev
+# K15 -- Sessions Greetings
+# 2020-12-14
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/") #, methods=['GET', 'POST'])
+def disp_loginpage():
+    return render_template( 'login.html' )
+
+@app.route("/welcome", methods=['GET', 'POST']) #, methods=['GET', 'POST'])
+def disp_welcomepage():
+    user = 'abracadabra'
+    password = '123456'
+
+    if request.args['username'] != user or request.args['password'] != password:
+            return ('Invalid Login Attempt. Please Try Again')
+
+    return render_template( 'welcome.html', user = request.args['username'])
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
