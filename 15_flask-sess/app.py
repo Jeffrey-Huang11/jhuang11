5bd4a929aa7f4a4b0cec0939edffa5fa8cc508db
Jeffrey Huang <71650487+Jeffrey-Huang11@users.noreply.github.com>
2020-12-12 20:34:37 -0500

    update
    updated 15

--- 15_flask-sess/app.py
+++ 15_flask-sess/app.py
@@ -2,38 +2,41 @@
 # SoftDev
 # K15 -- Sessions Greetings
 # 2020-12-14
-
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
-from flask import sessions
-import secrets
+from flask import session
+import os
+
+#the conventional way:
+#from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
-secret_key = secrets.token_hex(16)
-app.secret_key= secret_key
+app.secret_key = os.urandom(32) #Need this, if we didn't include this it would produce a runtime error
 
 @app.route("/") #, methods=['GET', 'POST'])
 def disp_loginpage():
-    return render_template( 'login.html' )
+    if 'username' in session: #Checks if the user is logged in
+        return render_template('response.html', user = 'abc')
+    else:
+        return render_template( 'login.html')
 
-@app.route("/", methods=['GET', 'POST']) #, methods=['GET', 'POST'])
-def disp_welcomepage():
+@app.route("/auth") # , methods=['GET', 'POST'])
+def welcome():
+    username = "abcd"
+    password = "1234"
+    if request.args['username'] == username:
+        if request.args['password'] == password:
+            session["username"] = username
+            return render_template('response.html', user = username)
+    else:
+        return render_template('invalid.html')
+    return render_template ( 'response.html')  #response to a form submission
 
-    username = 'abracadabra'
-    password = '123456'
-
-    if request.args['username'] != username and request.args['password'] != password:
-        return (''' <h1> Invalid Login Attempt. Bad juju Human \(^-^)/ </h1>
-                    <a href="http://127.0.0.1:5000/">Go Back</a> ''')
-    if request.args['username'] != username:
-        return (''' <h1> Invalid Login Attempt. Hint: Check your Username (>-<) </h1>
-                   <a href="http://127.0.0.1:5000/">Go Back</a> ''')
-    if request.args['password'] != password:
-        return (''' <h1> Invalid Login Attempt. Hint: Check your Password (>-<) </h1>
-                   <a href="http://127.0.0.1:5000/">Go Back</a> ''')
-
-    return render_template( 'welcome.html', user = request.args['username'])
+@app.route("/logout")
+def logout():
+    session.pop('username', None) #removes the session
+    return render_template('login.html')
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
