5dbe2f2b1852fe35b74a51f25412c589eefaf36d
Jeffrey Huang <71650487+Jeffrey-Huang11@users.noreply.github.com>
2021-04-12 15:06:16 -0400

    updated

---
+++ 21_js/intro.js
@@ -0,0 +1,43 @@
+/* Team Jam Juice (Jonathan Lee, Jeffrey Huang)
+SoftDev
+K21 - - Get Scripty
+2021-04-09 */
+
+function factI(n) { //returns factorial of n, computed iteratively.
+  for (i = n - 1; i > 0; i--) {
+    n = n * i;
+  }
+  return n
+}
+
+console.log(factI(2));
+
+function factR(n) { //returns factorial of n, computed recursively.
+  if (n == 1) return 1;
+  else return n * factR(n-1);
+}
+
+console.log(factI(2));
+
+function fibI(n) { //returns the nth Fibonacci number, computed iteratively.
+  var ans = 0;
+  var pri = 0;
+  var sec = 1;
+  if (n == 2) return 1;
+  for (i = 2; i < n; i++) {
+    ans = pri + sec;
+    pri = sec;
+    sec = ans;
+  }
+  return ans;
+}
+
+console.log(fibI(2));
+
+function fibR(n) { //returns the nth Fibonacci number, computed recursively.
+  if (n == 1) return 0;
+  if (n == 2) return 1;
+  else return fibR(n-1) + fibR(n-2);
+}
+
+console.log(fibR(2));
