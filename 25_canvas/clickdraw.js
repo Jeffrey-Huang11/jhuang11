1fbce4399c026d0667bf1df7f322ecf9b0d456ce
Jeffrey Huang <71650487+Jeffrey-Huang11@users.noreply.github.com>
2021-05-03 17:24:47 -0400

    updated

--- 25_canvas/clickdraw.js
+++ 25_canvas/clickdraw.js
@@ -3,18 +3,19 @@ var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "#ff0000";
 ctx.fillRect(50,50,100,200);
+
+//clears the canvas
 function doit() {
   ctx.clearRect(0,0,600,600);
   console.log("clicked clear");
 }
 
-
-
-function myFunction() {
+//toggles rectangle or dot draw mode if mouse within canvas
+function no() {
   var x = document.getElementById("Toggle");
   if (x.innerHTML === "Hello") {
-    x.innerHTML = "Swapped text!";
+    x.innerHTML = "Rectangle Mode";
   } else {
-    x.innerHTML = "Hello";
+    x.innerHTML = "Dot Mode";
   }
 }
