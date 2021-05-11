c28a321b3d9d5d3fd08a91673909e0349b93ae44
Jeffrey Huang <71650487+Jeffrey-Huang11@users.noreply.github.com>
2021-05-10 20:29:36 -0400

    updated

--- 27_canvas-anim/animate.js
+++ 27_canvas-anim/animate.js
@@ -1,15 +1,15 @@
-//Team Bubblegum Bears (Jeffrey Huang, Arib Chowdhury)
+//Team Uno (Jeffrey Huang)
 //SoftDev
-//K26 -- They lock us in the tower whenever we get caught
-//2021-05-05
+//K27 -- Guarding Against Burnout
+//2021-05-10
 
 // model for HTML5 canvas-based animation
-
 
 //access canvas and buttons via DOM
 var c =  document.getElementById("playground")// GET CANVAS
 var dotButton = document.getElementById("buttonCircle") // GET DOT BUTTON
 var stopButton = document.getElementById("buttonStop") // GET STOP BUTTON
+var dvdButton = document.getElementById("buttonDVD") // GET DVD BUTTON
 var ctx = c.getContext("2d");// YOUR CODE HERE
 
 //set fill color to team color
@@ -19,35 +19,34 @@ var requestID;  //init global var for use with animation frames
 
 //var clear = function(e) {
 var clear = (e) => {
-
   console.log("clear invoked...")
   ctx.clearRect(0,0,500,500);
 };
 
 var radius = 0;
 var growing = true;
+var logo = new Image();
+logo.src = "logo_dvd.jpg";
+var xx = 25;
+var yy = 25;
+var collision = false;
 
 //var drawDot = function() {
 var drawDot = () => {
   console.log("drawDot invoked...")
   dotButton.disabled = true;
   clear();
-
-  // YOUR CODE HERE
   if (growing) {
     radius+=1;
     if (radius == 250) growing=false;
   }
-
   else {
     radius-=1;
     if (radius == 0) growing=true;
   }
-
   ctx.beginPath();
   ctx.arc(250, 250, radius, 0, 2 * Math.PI); // draws a circle
   ctx.fill();
-
   requestID = window.requestAnimationFrame(drawDot);
 };
 
@@ -57,8 +56,26 @@ var stopIt = () => {
   console.log( requestID );
   dotButton.disabled = false;
   window.cancelAnimationFrame(requestID);
-
 };
 
+//var dvdIt = function() {
+var dvdIt = () => {
+  clear();
+  window.cancelAnimationFrame(requestID);
+  ctx.drawImage(logo, xx, yy, 75, 50);
+  if (collision) {
+    xx-=2;
+    yy-=2;
+  }
+  else {
+    xx+=2;
+    yy+=2;
+    if (xx > 475) collision = true;
+  }
+  requestID = window.requestAnimationFrame(dvdIt);
+}
+
+
 dotButton.addEventListener( "click", drawDot);
-stopButton.addEventListener( "click",  stopIt );
+stopButton.addEventListener( "click", stopIt);
+dvdButton.addEventListener( "click", dvdIt);
