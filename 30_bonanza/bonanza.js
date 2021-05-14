83156bc0d2c5874ab4182b2bf5e06f4ee5bc4d91
Jeffrey Huang <71650487+Jeffrey-Huang11@users.noreply.github.com>
2021-05-14 17:42:46 -0400

    updated

--- 30_bonanza/bonanza.js
+++ 30_bonanza/bonanza.js
@@ -1,4 +1,4 @@
-//Team Worm Ooga - Dean Carey Ethan Machleder Jeffrey Huang
+//Team Worm Ooga - Team Worm Ooga - Dean Carey, Ethan Machleder, Jeffrey Huang
 //SoftDev
 //K30: JSorcery
 //2021-05-14
@@ -7,44 +7,85 @@
 var c =  document.getElementById("playground")// GET CANVAS
 var startButton = document.getElementById("buttonStart") // GET START BUTTON
 var stopButton = document.getElementById("buttonStop") // GET STOP BUTTON
-
+var restartButton = document.getElementById("buttonRestart") // GET RESTART BUTTON
 var ctx = c.getContext("2d");// YOUR CODE HERE
 
-//set fill color to team color
-ctx.fillStyle = "blue"; // YOUR CODE HERE
+stopButton.disabled = true;
+restartButton.disabled = true;
 
-var loc = [500,500];
-var mv = [100,100];
+var loc = [500,500]; //location of snake head
+var mv = [50,50]; //velocity
 
-var logo = new Image();
-logo.src = "snake.jpg";
-logo.onload = function(){ctx.drawImage(logo,500,500,100,100)};
+var snake = new Image();
+snake.src = "snake.jpg";
+snake.onload = function(){ctx.drawImage(snake,500,500,50,50)};
 
+var apple = new Image();
+apple.src = "apple.jpg";
+apple.onload = function(){ctx.drawImage(apple,100,100,50,50)};
 
 var runIt = () => {
   console.log("run invoked...")
   startButton.disabled = true;
-  loc = [loc[0]+mv[0],loc[1]]; //moving to left originally
-  ctx.drawImage(logo,loc[0],loc[1],100,100);
-  requestID = window.requestAnimationFrame(dvdIt);
+  stopButton.disabled = false;
+  restartButton.disabled = false;
+  ctx.clearRect(loc[0],loc[1],100,100); // clears previous snake head
+  loc = [loc[0]+mv[0],loc[1]]; //moving to left when starting
+  if (loc[0] < 0 | loc[0] > 1000) {
+    alert("You Died");
+    restart();
+    return;
+  }
+  if (loc[1] < 0 | loc[1] > 1000) {
+    alert("You Died");
+    restart();
+    return;
+  }
+  ctx.drawImage(snake,loc[0],loc[1],50,50);
+  requestID = window.requestAnimationFrame(runIt);
 };
 
 c.addEventListener('keydown', function(event) {
   const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
-    if (loc[1] == 0 | loc[1] == 420) {
-      alert("You Died");
-      console.log("clear invoked...")
-      ctx.clearRect(0,0,1000,1000);
-    }
+
 });
+
+
+document.onkeydown = checkKey;
+function checkKey(e) {
+    e = e || window.event;
+    if (e.keyCode == '38') { // up arrow
+
+    }
+    else if (e.keyCode == '40') { // down arrow
+
+    }
+    else if (e.keyCode == '37') { // left arrow
+
+    }
+    else if (e.keyCode == '39') { // right arrow
+
+    }
+
+}
+
 
 var stopIt = () => {
     console.log("stopIt invoked...")
-    console.log( requestID );
+    console.log(requestID);
     startButton.disabled = false;
-    dvdButton.disabled = false;
     window.cancelAnimationFrame(requestID);
 };
+
+var restart = () => {
+    console.log("restart invoked...")
+    ctx.clearRect(0,0,1000,1000);
+    ctx.drawImage(snake,500,500,50,50);
+    ctx.drawImage(apple,100,100,50,50);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    restartButton.disabled = true;
+}
 
 
 startButton.addEventListener( "click", runIt);
